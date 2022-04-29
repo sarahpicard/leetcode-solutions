6f458f22eb6be78f4ffaad21150933b627901d5c
sarahpicard <sarahannrey225@gmail.com>
2022-04-29 15:08:55 -0400

    stub up 03 longest substring

---
+++ EasyProblems/03 LongestSubstring.js
@@ -0,0 +1,25 @@
+// Problem #3
+// Longest Substring without Repeating Charcters
+
+// given a string (s), find the length of the longest substring without repeating characters
+
+// example 1
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+
+// example 2
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+
+// example 3
+// Input: s = "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+
+var lengthOfLongestSubstring = function(s) {
+
+}
